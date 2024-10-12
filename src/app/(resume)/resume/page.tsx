bc7ccae3f67413f2ce4a827d1bbbf689536d178d
za0012 <zinzeek@gmail.com>
2024-10-12 14:45:17 +0900

    첫커밋

--- src/app/(resume)/resume/page.tsx
+++ src/app/(resume)/resume/page.tsx
@@ -1,6 +1,19 @@
+'use client'
+
+import { createCli } from '@/api/subabase';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const ResumePage = () => {
+  const supabase = 
+  //Tanstack Query를 사용하여 데이터 가져오기
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['resume'],
+    queryFn: async () => {
+      const { data: post_detail, error } = await supabase.from('post_detail').select();
+    }
+  });
+
   return <div>ResumePage</div>;
 };
 
