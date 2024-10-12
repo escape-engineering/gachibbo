d99b9574a57a011f5a1e42fb4c976bef99ddb3db
play93 <95739833+play93@users.noreply.github.com>
2024-10-13 00:52:17 +0900

    fix : 회원가입(멘토) 유효성 검사 결과가 화면에 표시되지 않는 문제 수정

--- src/app/(auth)/signup/mento/page.tsx
+++ src/app/(auth)/signup/mento/page.tsx
@@ -4,7 +4,7 @@ import Button from '@/app/_components/common/Button';
 import { createClient } from '@/utils/supabase/client';
 import { zodResolver } from '@hookform/resolvers/zod';
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
@@ -24,38 +24,65 @@ const MentoSignUpPage = () => {
 
   //zod
   const signUpSchema = z.object({
-    user_id: z.string().min(1, '아이디는 필수입니다.'),
+    user_id: z
+      .string()
+      .min(1, '아이디는 필수입니다.')
+      .regex(/^[a-z0-9]{4,30}$/, '영문 소문자 또는 영문+숫자 조합 4~30자리를 입력해주세요.'),
     user_pw: z.string().min(6, '비밀번호는 최소 6자리 이상이어야 합니다.'),
+    // .regex(
+    //   /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/,
+    //   "영문+숫자+특수문자(! @ # $ % & * ?) 조합 8~15자리를 입력해주세요."
+    // ),
     email: z.string().email('유효한 이메일을 입력하세요.').min(1, '이메일은 필수입니다.'),
     user_name: z.string().min(1, '닉네임은 필수입니다.'),
     image_url: z.string().optional() // 이미지 URL은 선택 사항
   });
 
-  // 인풋에 입력한 값 실시간으로 확인
-  const handleInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
+  // // 인풋에 입력한 값 실시간으로 확인
+  // const handleInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const { name, value } = e.target;
+  //   setFormData((prev) => ({ ...prev, [name]: value }));
+  // };
 
-  // 경력 실시간 확인
-  const handleSelectChange = async (value: string) => {
-    setFormData((prev) => ({ ...prev, mento_work_experience: value }));
-  };
+  // // 경력 실시간 확인
+  // const handleSelectChange = async (value: string) => {
+  //   setFormData((prev) => ({ ...prev, mento_work_experience: value }));
+  // };
 
-  // 현직여부 실시간 확인
-  const handleRadioChange = async (value: boolean) => {
-    setFormData((prev) => ({ ...prev, mento_current: value }));
-  };
+  // // 현직여부 실시간 확인
+  // const handleRadioChange = async (value: boolean) => {
+  //   setFormData((prev) => ({ ...prev, mento_current: value }));
+  // };
 
   // 리액트 훅 폼으로 유효성 검사
-  const { register, handleSubmit, formState } = useForm({
+  const { register, handleSubmit, formState, setValue } = useForm({
     mode: 'onChange',
-    defaultValues: formData,
+    defaultValues: {
+      user_id: '',
+      user_pw: '',
+      user_type: 'mentee',
+      email: '',
+      user_name: '',
+      image_url: '',
+      mento_current: false,
+      mento_work_experience: ''
+    },
     resolver: zodResolver(signUpSchema)
   });
 
+  type FormData = {
+    user_id: string;
+    user_pw: string;
+    user_type: string;
+    email: string;
+    user_name: string;
+    image_url: string;
+    mento_current: boolean;
+    mento_work_experience: string;
+  };
+
   // 폼 제출 함수
-  const onSubmit = async () => {
+  const onSubmit: SubmitHandler<FormData> = async (formData) => {
     const supabase = createClient();
 
     // Supabase에 사용자 등록
@@ -69,8 +96,8 @@ const MentoSignUpPage = () => {
     }
 
     if (!data.user) {
-      console.error('사용자 생성에 실패했습니다. 데이터가 없습니다.');
-      return; // 데이터가 없을 경우 종료
+      console.error('이미 가입된 정보입니다.');
+      return;
     }
 
     const { error } = await supabase.from('auth').insert({
@@ -99,56 +126,27 @@ const MentoSignUpPage = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="user_id">아이디</label>
-          <input
-            {...register('user_id')}
-            id="user_id"
-            type="text"
-            name="user_id"
-            value={formData.user_id}
-            onChange={handleInputChange}
-            required
-          />
+          <input {...register('user_id')} id="user_id" type="text" required />
+          {formState.errors.user_id && <span>{formState.errors.user_id.message}</span>}
         </div>
         <div>
           <label htmlFor="user_pw">비밀번호</label>
-          <input
-            {...register('user_pw')}
-            id="user_pw"
-            type="password"
-            name="user_pw"
-            value={formData.user_pw}
-            onChange={handleInputChange}
-            required
-          />
+          <input {...register('user_pw')} id="user_pw" type="password" required />
+          {formState.errors.user_pw && <span>{formState.errors.user_pw.message}</span>}
         </div>
         <div>
           <label htmlFor="user_name">닉네임</label>
-          <input
-            {...register('user_name')}
-            id="user_name"
-            type="text"
-            name="user_name"
-            value={formData.user_name}
-            onChange={handleInputChange}
-            required
-          />
+          <input {...register('user_name')} id="user_name" type="text" required />
+          {formState.errors.user_name && <span>{formState.errors.user_name.message}</span>}
         </div>
         <div>
           <label htmlFor="email">이메일</label>
-          <input
-            {...register('email')}
-            id="email"
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            required
-          />
+          <input {...register('email')} id="email" type="email" required />
           {formState.errors.email && <span>{formState.errors.email.message}</span>}
         </div>
         <div>
           <label>경력</label>
-          <Select onValueChange={handleSelectChange}>
+          <Select onValueChange={(value) => setValue('mento_work_experience', value)}>
             <SelectTrigger className="w-[180px]">
               <SelectValue placeholder="경력을 선택해 주세요." />
             </SelectTrigger>
@@ -161,7 +159,7 @@ const MentoSignUpPage = () => {
         </div>
         <div>
           <label>현직에서 일하고 계신가요?</label>
-          <RadioGroup onValueChange={(value) => handleRadioChange(value === 'yes')} defaultValue="no">
+          <RadioGroup onValueChange={(value) => setValue('mento_current', value === 'yes')} defaultValue="no">
             <div className="flex items-center space-x-2">
               <RadioGroupItem value="yes" id="yes" />
               <Label htmlFor="yes">예</Label>
