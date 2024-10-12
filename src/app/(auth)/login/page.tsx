0ed9a05fa15893b6209b786d1d715fd41321492a
play93 <95739833+play93@users.noreply.github.com>
2024-10-12 13:57:38 +0900

    feat : 로그인 폼 제출 함수 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,19 @@
 'use client';
 
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import Link from 'next/link';
+import Button from '@/app/_components/common/Button';
+import { createClient } from '@/utils/supabase/client';
 
 const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    user_id: '',
+    user_pw: ''
+  });
+
   const signInSchema = z.object({
     user_id: z.string().min(1, {
       message: '아이디는 필수입니다.'
@@ -27,14 +34,28 @@ const LoginPage = () => {
   });
 
   // 폼 제출 함수
-  const onSubmit = async () => {};
+  const onSubmit = async () => {
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: formData.user_id,
+      password: formData.user_pw
+    });
+    if (error) console.error(error);
+  };
 
   return (
     <section>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input {...register('user_id')} type="text" name="id" placeholder="아이디" required />
         <input {...register('user_pw')} type="password" name="password" placeholder="비밀번호" required />
-        <button disabled={!formState.isValid}>로그인</button>
+        <Button
+          onClick={() => {
+            console.log('로그인 폼 제출됨');
+          }}
+          type="submit"
+        >
+          로그인
+        </Button>
       </form>
       <div>
         <p>아직 회원이 아니신가요?</p> <Link href={'/signup'}>회원가입</Link>
